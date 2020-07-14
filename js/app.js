f01d2998e82418f1a6d02dc23887d883b247a1bc
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-14 01:22:15 -0400

    spread operator

---
+++ js/app.js
@@ -0,0 +1,54 @@
+var x = {
+    "taco": "bell",
+    "burrito": 4,
+    "beefy": ["cheese", "nachos"]
+};
+    console.log(x["taco"]);
+//    
+
+var y = {
+    "boy": "friend",
+    "girl": 80,
+    "happy": ["birthday", "dear"]
+};
+    console.log(y.happy);
+//
+
+var z = {
+    "GD": 69,
+    "Young": "Bae",
+    "Big": ["Bang", "Yo"]
+};
+    console.log(z["GD"]);
+
+    // Functions with Arrays as arguments
+
+    // function homeBoy(burger, king) {
+    //     console.log(burger, king);
+    // }
+
+    // homeBoy("French", "Fries");
+
+
+    //
+// var ice = [2,4,6,8];
+
+// var total = 0;
+
+//     function cake(arg1, arg2) {
+// for (var i = 0; i < ice.length; i++) {
+//     total += ice[i]
+//     console.log(ice[i]);
+// }
+//     }
+//     console.log(total);
+
+var ice = [2,4,6,8]
+//
+var total = 0;
+
+for (var i = 0; i < ice.length; i++) {
+    total += ice[i]
+    console.log(ice[i]);
+}
+console.log(total);
